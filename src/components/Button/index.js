4c8258b2b6de2367105b9a5adb0dfd8c98154cd4
dungggg1203 <dungcongnghiep4@gmail.com>
2023-07-22 22:47:53 +0700

    phan2

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,12 +4,36 @@ import styles from './Button.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Button({ to, primary, href, children, onClick, ...passProps }) {
+function Button({
+  to,
+  primary = false,
+  outline = false,
+  text = false,
+  disabled = false,
+  rounded = false,
+  small = false,
+  large = false,
+  href,
+  leftIcon,
+  rightIcon,
+  children,
+  onClick,
+  className,
+  ...passProps
+}) {
   let Comp = 'button';
   const props = {
     onClick,
     ...passProps,
   };
+  // loại bỏ sự kiện
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
 
   if (to) {
     props.to = to;
@@ -19,12 +43,23 @@ function Button({ to, primary, href, children, onClick, ...passProps }) {
     Comp = 'a';
   }
 
+  const wrapperBtn = cx('wrapper-btn');
   const classes = cx('wrapper', {
+    [className]: className,
     primary,
+    outline,
+    small,
+    text,
+    rounded,
+    disabled,
+    large,
+    [wrapperBtn]: true,
   });
   return (
     <Comp className={classes} {...props}>
-      <span>{children}</span>
+      {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+      <span className={cx('title')}>{children}</span>
+      {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
     </Comp>
   );
 }
--- src/components/GlobalStyle/Layout/DefaultLayout/Header/index.js
+++ src/components/GlobalStyle/Layout/DefaultLayout/Header/index.js
@@ -7,10 +7,38 @@ import { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'; // Thêm dòng này
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+  faSignIn,
+  faEllipsisVertical,
+  faEarthAsia,
+  faCircleQuestion,
+  faKeyboard,
+} from '@fortawesome/free-solid-svg-icons'; // Thêm dòng này
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: 'English',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: 'Trợ giúp & phản hồi',
+    to: '/feedback',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: 'Bàn phím ',
+  },
+];
+
 function Header() {
   const [searchResult, setSearchResult] = useState([]);
   useEffect(() => {
@@ -53,7 +81,14 @@ function Header() {
           </div>
         </Tippy>
         <div className={cx('actions')}>
-          <Button>Log in</Button>
+          <Button text>Upload</Button>
+          <Button primary>Log in</Button>
+
+          <Menu items={MENU_ITEMS}>
+            <button className={cx('more-btn')}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
         </div>
       </div>
     </header>
